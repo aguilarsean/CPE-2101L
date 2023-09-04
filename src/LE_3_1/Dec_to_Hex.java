750a1dd8289abeef3ebb35165a5cedd11bce7352
skaguilar <skaguilar@CN-S20.cnlabserver.com>
2023-09-04 16:02:01 +0800

    finished

--- src/LE_3_1/Dec_to_Hex.java
+++ src/LE_3_1/Dec_to_Hex.java
@@ -1,7 +1,18 @@
 package LE_3_1;
 
+import java.util.Scanner;
+
 public class Dec_to_Hex {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.print("Enter a decimal number: ");
+		int decimalNumber = scanner.nextInt();
+		
+		String hexString = Integer.toHexString(decimalNumber).toUpperCase();
+		
+		System.out.print("Hexadecimal number: " + hexString);
+		
+		scanner.close();
 	}
 }
--- src/LE_3_1/Heads_or_Tails.java
+++ src/LE_3_1/Heads_or_Tails.java
@@ -2,6 +2,18 @@ package LE_3_1;
 
 public class Heads_or_Tails {
 	public static void main(String[] args) {
+		int heads = 0;
+		int tails = 0;
 		
+		for (int i = 0; i < 2000000; i++) {
+			int flip = (int) (Math.random() * 2);
+			if (flip == 1) {
+				heads++;
+			} else {
+				tails++;
+			}
+		}
+		System.out.println("Heads: " + heads);
+		System.out.println("Tails: " + tails);
 	}
 }
