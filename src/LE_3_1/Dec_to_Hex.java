c4cc6f97066f7c976eec85063c3629fc8814c10a
Sean Aguilar <seanaguilar698@gmail.com>
2023-09-05 20:29:28 +0800

    change convertion

--- src/LE_3_1/Dec_to_Hex.java
+++ src/LE_3_1/Dec_to_Hex.java
@@ -5,14 +5,31 @@ import java.util.Scanner;
 public class Dec_to_Hex {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.print("Enter a decimal number: ");
 		int decimalNumber = scanner.nextInt();
-		
-		String hexString = Integer.toHexString(decimalNumber).toUpperCase();
-		
+
+		String hexString = decimalToHex(decimalNumber).toUpperCase();
+
 		System.out.print("Hexadecimal number: " + hexString);
-		
+
 		scanner.close();
+	}
+
+	public static String decimalToHex(int decimalNumber) {
+		if (decimalNumber == 0) {
+			return "0";
+		}
+
+		StringBuilder hexStringBuilder = new StringBuilder();
+
+		while (decimalNumber > 0) {
+			int remainder = decimalNumber % 16;
+			char hexDigit = (remainder < 10) ? (char) ('0' + remainder) : (char) ('A' + remainder - 10);
+			hexStringBuilder.insert(0, hexDigit);
+			decimalNumber /= 16;
+		}
+
+		return hexStringBuilder.toString();
 	}
 }
