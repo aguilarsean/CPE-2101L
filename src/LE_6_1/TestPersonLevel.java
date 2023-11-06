b7a2a9ad4833224ed5efd0d4296893984a4b7c83
skaguilar <skaguilar@CN-S20.cnlabserver.com>
2023-11-06 14:50:35 +0800

    LE 6.1

---
+++ src/LE_6_1/TestPersonLevel.java
@@ -0,0 +1,66 @@
+package LE_6_1;
+
+import java.sql.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+class MyDate {
+	private int year;
+	private int month;
+	private int day;
+	
+	public MyDate() {
+		Calendar calendar = Calendar.getInstance();
+		this.year = calendar.get(Calendar.YEAR);
+		this.month = calendar.get(Calendar.MONTH);
+		this.day = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		System.out.println(year + " " + month + " " + day + " " );
+	}
+	
+	public MyDate(long elapsedTime) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeInMillis(elapsedTime);
+		this.year = calendar.get(Calendar.YEAR);
+		this.month = calendar.get(Calendar.MONTH);
+		this.day = calendar.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public MyDate(int year, int month, int day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+	}
+	
+	public int getYear() {
+		return this.year;
+	}
+	
+	public int getMonth() {
+		return this.month;
+	}
+	
+	public int getDay() {
+		return this.day;
+	}
+	
+	public void setDate(long elapsedTime) {
+		long totalMilliseconds = elapsedTime;
+		long totalSecond = totalMilliseconds / 1000;
+		long currentSecond = totalSecond % 60;
+		long totalMinutes = totalSecond / 60;
+		long currentMinute = totalMinutes % 60;
+		long totalHours = totalMinutes / 60;
+		long currentHour = totalHours % 60;
+		
+		int yearsSince1970 = (int) (totalHours / 8760);
+		years = 1970 + yearsSince1970
+		
+	}
+}
+
+public class TestPersonLevel {
+	public static void main(String[] args) {
+		MyDate myDate = new MyDate();
+	}
+}
